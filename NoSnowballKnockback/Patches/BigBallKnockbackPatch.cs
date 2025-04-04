e538b1fef0c3c52da0cc8933527c7dcea585f15d
MonkyGT <monkygtstudios@gmail.com>
2025-04-03 21:29:32 -0400

    Initial Commit

---
+++ NoSnowballKnockback/Patches/BigBallKnockbackPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NoSnowballKnockback.Patches
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.GTPlayer), "ApplyKnockback")]
+    public class BigBallKnockbackPatch
+    {
+        public static bool enable = true;
+
+        public static bool Prefix(Vector3 direcy, float speedy)
+        { 
+            if (enable)
+            {
+                direcy = Vector3.zero;
+                speedy = 0f;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ NoSnowballKnockback/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace NoSnowballKnockback
+{
+    [BepInPlugin("monky.nosnowballknockback", "NoSnowballKnockback", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        
+        bool enabled;
+
+        void Start()
+        { 
+            Harmony.CreateAndPatchAll(GetType().Assembly, "monky.nosnowballknockback");
+        }
+
+        void OnDisable()
+        {
+            enabled = false;
+        }
+
+        void OnEnable()
+        {
+            enabled = true;
+        }
+
+        void Update()
+        {
+            if (NetworkSystem.Instance.GameModeString.Contains("MODDED") && enabled)
+            {
+                Patches.BigBallKnockbackPatch.enable = true;
+            }
+            else
+            {
+                Patches.BigBallKnockbackPatch.enable = false;
+            }
+        }
+    }
+}
